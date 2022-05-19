63fb8a0ea1ce060f99cbb9e03ca170fdce781815
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-05-19 15:41:34 +0300

    Retrieve Login func to a separate comp

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import AddMovie from './AddMovie';
 import SelectLanguage from './SelectLanguage';
+import Login from './Login';
 
 function Options() {
   return (
     <div>
       <AddMovie />
       <SelectLanguage />
-      <li>Login</li>
+      <Login />
       <li>Join TMDB</li>
       <li>Search</li>
     </div>
