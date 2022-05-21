d5adfbc3eeece34ff879b051879e127de8d95946
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-05-21 15:47:45 +0300

    Add common fun to capitalize text

---
+++ src/utils.ts
@@ -0,0 +1,10 @@
+function capitalize(phrase:string) {
+  return phrase.split(' ').map((word) => {
+    if (word === 'tv' || word === 'api') {
+      return word.toUpperCase();
+    }
+    return `${word[0].toUpperCase()}${word.slice(1)}`;
+  }).join(' ');
+}
+
+export default capitalize;
