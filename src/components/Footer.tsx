4f7cec42fc74bfcb2d600cc2145defe8f31e372a
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-05-23 14:26:39 +0300

    Add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="p-4 footer">
+      <h2>The Movie Database Clone</h2>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Header from './header/Header';
+import Footer from './Footer';
 
 function MainPage() {
-  return <div style={{ height: '2000px' }}><Header /></div>;
+  return (
+    <div style={{ height: '2000px' }}>
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default MainPage;
