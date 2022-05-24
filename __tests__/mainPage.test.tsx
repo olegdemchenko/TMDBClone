331bc82d1d6439b04b455bea74bf0c5681f375e9
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-05-24 13:04:46 +0300

    Add tests to check correctness of Search comp impl

--- __tests__/mainPage.test.tsx
+++ __tests__/mainPage.test.tsx
@@ -7,6 +7,8 @@ import {
 } from '@testing-library/react';
 import React from 'react';
 import App from '../src/components/App';
+import server from '../src/mocks/server';
+import Search from '../src/mocks/testQueries';
 
 test('check header behavior', async () => {
   render(<App />);
@@ -19,6 +21,54 @@ test('check header behavior', async () => {
   fireEvent.scroll(window, { target: { scrollY: -100 } });
   await waitFor(() => {
     expect(header).toHaveClass('visible');
+  });
+});
+
+describe('check fetching data from API', () => {
+  beforeAll(() => server.listen());
+
+  afterEach(() => server.resetHandlers());
+
+  afterAll(() => server.close());
+
+  test('check blocking search elems after search request is sent', async () => {
+    render(<App />);
+    const searchInput = screen.getByRole('textbox', { name: /search input/i });
+    const searchBtn = screen.getByRole('button', { name: /search/i });
+    fireEvent.change(searchInput, { targer: { value: Search.MultiSearch } });
+    fireEvent.click(searchBtn);
+    await waitFor(() => {
+      expect(searchBtn).toBeDisabled();
+    });
+    await waitFor(() => {
+      expect(searchInput).toBeDisabled();
+    });
+  });
+
+  test('check successful search', async () => {
+    const searchInput = screen.getByRole('textbox', { name: /search input/i });
+    const searchBtn = screen.getByRole('button', { name: /search/i });
+    fireEvent.change(searchInput, { targer: { value: Search.MultiSearch } });
+    fireEvent.click(searchBtn);
+    await waitFor(() => {
+      expect(window.location.pathname).toBe('/search');
+    });
+    await waitFor(() => {
+      expect(screen.getByRole('heading', { name: /search results/i })).toBeInTheDocument();
+    });
+  });
+
+  test('check error handling after search request is sent', async () => {
+    const searchInput = screen.getByRole('textbox', { name: /search input/i });
+    const searchBtn = screen.getByRole('button', { name: /search/i });
+    fireEvent.change(searchInput, { targer: { value: Search.MultiSearchError } });
+    fireEvent.click(searchBtn);
+    await waitFor(() => {
+      expect(window.location.pathname).toBe('/');
+    });
+    await waitFor(() => {
+      expect(screen.getByRole('alert', { name: /search error/i })).toBeInTheDocument();
+    });
   });
 });
 
