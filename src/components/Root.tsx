32af597cd7fd9da5e11cf2cfec081cd435f2e396
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-05-25 18:29:00 +0300

    Extract SearchResults comp into a separate feature

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -6,7 +6,7 @@ import {
 } from 'react-router-dom';
 import App from './App';
 import Main from './MainPage';
-import SearchResults from '../features/search/SearchResults';
+import SearchResults from '../features/searchResults/SearchResults';
 
 function Root() {
   return (
