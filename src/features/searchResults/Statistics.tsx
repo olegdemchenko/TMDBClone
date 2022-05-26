8d01cb06074b2f85da3af76b4b8b1800a8678d73
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-05-26 16:58:40 +0300

    Implement search res statistic UI

--- src/features/searchResults/Statistics.tsx
+++ src/features/searchResults/Statistics.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import cn from 'classnames';
+import _ from 'lodash';
+import capitalize from '../../utils';
 
 function Statistics() {
-  return <div>Statistics</div>;
+  const categories = ['people', 'movies', 'tv shows', 'collections', 'companies', 'keywords', 'networks'];
+  return (
+    <div className="statistics">
+      <h6 className="fw-bold p-4 text-white">Search Results</h6>
+      {categories.map((category, index) => {
+        const containerClass = cn('d-flex', 'justify-content-between', 'px-4', 'py-2', 'category', {
+          primary: index === 0,
+        });
+        return (
+          <div key={_.uniqueId()} className={containerClass}>
+            <span className="category-name">{capitalize(category)}</span>
+            <span className="category-count">0</span>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Statistics;
