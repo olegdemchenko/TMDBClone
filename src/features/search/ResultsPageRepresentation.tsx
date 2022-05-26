cefc09bfb01e0e4e92084848d7e02b8a519d4ad1
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-05-26 11:43:29 +0300

    Extract wrappers from Search comp to Search Result

--- src/features/search/ResultsPageRepresentation.tsx
+++ src/features/search/ResultsPageRepresentation.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { SearchUIProps } from './MainPageRepresentation';
@@ -10,20 +9,16 @@ function ResultsPageRepresentation({
   error,
 }: SearchUIProps) {
   return (
-    <div className="border-bottom">
-      <Container className="px-4">
-        <Form className="position-relative search-results" onSubmit={onSubmit}>
-          <Form.Control
-            className="border-0 ps-4"
-            name="search input"
-            onChange={onChange}
-            isInvalid={!!error}
-          />
-          <Form.Control.Feedback type="invalid">{error}</Form.Control.Feedback>
-          <Button variant="light" className="border-0 p-0 position-absolute fs-4 top-0 start-0" type="submit">{'\u2315'}</Button>
-        </Form>
-      </Container>
-    </div>
+    <Form className="position-relative search-results" onSubmit={onSubmit}>
+      <Form.Control
+        className="border-0 ps-4"
+        name="search input"
+        onChange={onChange}
+        isInvalid={!!error}
+      />
+      <Form.Control.Feedback type="invalid">{error}</Form.Control.Feedback>
+      <Button variant="light" className="border-0 p-0 position-absolute fs-4 top-0 start-0" type="submit">{'\u2315'}</Button>
+    </Form>
   );
 }
 
--- src/features/searchResults/SearchResults.tsx
+++ src/features/searchResults/SearchResults.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 import Search from '../search/Search';
 
 function SearchResults() {
-  return <div><Search mode="results" /></div>;
+  return (
+    <div>
+      <div className="border-bottom">
+        <Container className="px-4">
+          <Search mode="results" />
+        </Container>
+      </div>
+    </div>
+  );
 }
 
 export default SearchResults;
