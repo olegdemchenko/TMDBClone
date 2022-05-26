5bea1dd7cceab327c5dc8cb5c9477af86907b0fd
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-05-26 12:25:40 +0300

    Implement SearchResults comp structure and styles

---
+++ src/features/searchResults/ResultsList.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ResultsList() {
+  return <div>List of results</div>;
+}
+
+export default ResultsList;
--- src/features/searchResults/SearchResults.tsx
+++ src/features/searchResults/SearchResults.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Search from '../search/Search';
+import Statistics from './Statistics';
+import ResultsList from './ResultsList';
 
 function SearchResults() {
   return (
     <div>
       <div className="border-bottom">
-        <Container className="px-4">
+        <Container fluid="lg" className="px-4">
           <Search mode="results" />
         </Container>
       </div>
+      <Container fluid="lg" className="px-4 d-flex">
+        <div className="statistics-wrapper">
+          <Statistics />
+        </div>
+        <div className="flex-grow-1 overflow-hidden">
+          <ResultsList />
+        </div>
+      </Container>
     </div>
   );
 }
