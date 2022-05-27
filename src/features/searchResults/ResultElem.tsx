2ff85eb9746d7ec2b007ab6c68d8ccf3e174b261
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-05-27 11:46:13 +0300

    Add text truncation to search res descriptions

--- src/features/searchResults/ResultElem.tsx
+++ src/features/searchResults/ResultElem.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import LinesEllipsis from 'react-lines-ellipsis';
 
 interface ResultElemProps {
   title: string;
@@ -21,7 +22,13 @@ function ResultElem({
       <div className="data-wrapper flex-grow-1">
         <h6 className="m-0 fs-5 fw-bold">{title}</h6>
         <p className="text-muted">{date}</p>
-        <p>{description}</p>
+        <LinesEllipsis
+          text={description}
+          maxLine="3"
+          ellipsis="..."
+          trimRight
+          basedOn="words"
+        />
       </div>
     </div>
   );
