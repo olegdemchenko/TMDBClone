ef160c6aa26ae3f35b57cbc5ead2f84d408e9245
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-05-30 12:24:19 +0300

    Add fetching search query res to Results comp

--- src/features/search/Results.tsx
+++ src/features/search/Results.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { useSearchParams } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
+import routes from '../../routes/routes';
 import Search from '.';
 import Statistics from './Statistics';
 import ResultsList from './ResultsList';
 
 function Results() {
+  const [params] = useSearchParams();
+  const searchQuery = params.get('query');
+
+  useEffect(() => {
+    async function fetchSearchQueryRes() {
+      const url = routes.getMultiSearch(searchQuery);
+      try {
+        const { data } = await axios.get(url);
+        console.log(data);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    fetchSearchQueryRes();
+  }, [searchQuery]);
+
   return (
     <div>
       <div className="border-bottom">
