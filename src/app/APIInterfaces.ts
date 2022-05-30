cca26c3df6530c51eac8cdcb5f384379dee0339a
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-05-30 15:38:04 +0300

    Make fields of interf related to search optional

--- src/app/APIInterfaces.ts
+++ src/app/APIInterfaces.ts
@@ -1,43 +1,43 @@
 interface MovieSearchInfo {
-  poster_path: string | null;
-  adult: boolean;
-  overview: string;
-  release_date: string;
-  original_date: string;
-  genre_ids: number[];
+  poster_path?: string | null;
+  adult?: boolean;
+  overview?: string;
+  release_date?: string;
+  original_date?: string;
+  genre_ids?: number[];
   id: number;
-  media_type: string;
-  original_language: string;
-  title: string;
-  backdrop_path: string;
-  popularity: number;
-  vote_count: number;
-  video: boolean;
-  vote_average: number;
+  media_type: 'movie';
+  original_language?: string;
+  title?: string;
+  backdrop_path?: string;
+  popularity?: number;
+  vote_count?: number;
+  video?: boolean;
+  vote_average?: number;
 }
 
 export interface TVSearchInfo {
-  poster_path: string | null;
-  popularity: number;
-  id: number;
-  overview: string;
-  backdrop_path: string | null;
-  media_type: string;
-  first_air_date: string;
-  origin_country: string[];
-  genre_ids: number[];
-  original_language: string;
-  vote_average: number;
+  poster_path?: string | null;
+  popularity?: number;
+  id?: number;
+  overview?: string;
+  backdrop_path?: string | null;
+  media_type: 'tv';
+  first_air_date?: string;
+  origin_country?: string[];
+  genre_ids?: number[];
+  original_language?: string;
+  vote_average?: number;
   vote_count: number;
-  name: string;
-  original_name: string;
+  name?: string;
+  original_name?: string;
 }
 
 interface PersonSearchInfo {
   profile_path: string | null;
   adult: boolean;
   id: number;
-  media_type: string;
+  media_type: 'person';
   known_for: TVSearchInfo | MovieSearchInfo;
   name: string;
   total_pages: number;
