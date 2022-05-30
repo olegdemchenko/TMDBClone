155ced5035e8bc139977f4fd7f9f29c2e3f95184
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-05-30 15:20:06 +0300

    Add fetching search results to Results component

--- src/features/search/Results.tsx
+++ src/features/search/Results.tsx
@@ -1,28 +1,64 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import axios, { AxiosError } from 'axios';
 import { useSearchParams } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
+import Spinner from 'react-bootstrap/Spinner';
+import Alert from 'react-bootstrap/Alert';
 import routes from '../../routes/routes';
 import Search from '.';
 import Statistics from './Statistics';
 import ResultsList from './ResultsList';
 
+enum SearchResultsState {
+  fetching = 'fetching',
+  success = 'success',
+  error = 'error',
+}
 function Results() {
+  const [state, setState] = useState<SearchResultsState>(SearchResultsState.fetching);
+
+  const [results, setResults] = useState({});
+
+  const [error, setError] = useState<string | null>(null);
+
   const [params] = useSearchParams();
   const searchQuery = params.get('query');
 
   useEffect(() => {
     async function fetchSearchQueryRes() {
       const url = routes.getMultiSearch(searchQuery);
+      setState(SearchResultsState.fetching);
       try {
         const { data } = await axios.get(url);
         console.log(data);
-      } catch (e) {
-        console.log(e);
+        setState(SearchResultsState.success);
+        setResults(data);
+      } catch (err) {
+        if (err instanceof AxiosError) {
+          setState(SearchResultsState.error);
+          setError(err.response?.data?.status_message);
+        }
+        throw err;
       }
     }
     fetchSearchQueryRes();
   }, [searchQuery]);
+
+  if (state === SearchResultsState.fetching) {
+    return (
+      <Container fluid="lg" className="d-flex vh-100 justify-content-center align-items-center">
+        <Spinner animation="border" />
+      </Container>
+    );
+  }
+
+  if (state === SearchResultsState.error) {
+    return (
+      <Container fluid="lg" className="py-4">
+        <Alert className="m-0" variant="danger">{error}</Alert>
+      </Container>
+    );
+  }
 
   return (
     <div>
