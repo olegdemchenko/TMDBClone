f9e6560841096b03d963fe1f2d907c890eef747b
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-05-31 17:53:01 +0300

    Change impl of ResultsList to render real data

--- src/features/search/ResultsList.tsx
+++ src/features/search/ResultsList.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
+import { MultiSearchResults, MediaTypes, PersonSearchInfo } from '../../app/APIInterfaces';
 import ResultElem from './ResultElem';
+import PersonInfo from './PersonInfo';
 
-function ResultsList() {
-  const example = {
-    title: 'Friends',
-    date: 'September 22, 1994',
-    logo: '',
-    description: 'Friends is an American television sitcom created by David Crane and Marta Kauffman, which aired on NBC from September 22, 1994, to May 6, 2004, lasting ten seasons. With an ensemble cast starring Jennifer Aniston, Courteney Cox, Lisa Kudrow, Matt LeBlanc, Matthew Perry and David Schwimmer, the show revolves around six friends in their 20s and 30s who live in Manhattan, New York City. The series was produced by Bright/Kauffman/Crane Productions, in association with Warner Bros. Television. The original executive producers were Kevin S. Bright, Kauffman, and Crane.',
-  };
+function ResultsList({ results }: MultiSearchResults) {
   return (
     <div className="ps-4">
-      {Array(10).fill(example).map((item) => (
-        <ResultElem
-          title={item.title}
-          date={item.date}
-          logo={item.logo}
-          description={item.description}
-        />
-      ))}
+      {results.map((res) => {
+        if (res.media_type === MediaTypes.movie || res.media_type === MediaTypes.tv) {
+          return (
+            <ResultElem
+              title={res.media_type === MediaTypes.movie ? res.title : res.name}
+              logo={res.poster_path}
+              date={res.media_type === MediaTypes.movie ? res.original_date : res.first_air_date}
+              description={res.overview}
+            />
+          );
+        }
+        return <PersonInfo info={res} />;
+      })}
     </div>
   );
 }
