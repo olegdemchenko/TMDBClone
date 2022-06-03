8cf264112559a685f90dcb63f3bc0a572adbe17a
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-06-03 17:20:15 +0300

    Add a placeholder to ResultsPage search field

--- src/features/search/ResultsPageSearch.tsx
+++ src/features/search/ResultsPageSearch.tsx
@@ -11,6 +11,7 @@ function ResultsPageSearch({
       <Form.Control
         className="border-0 ps-4"
         name="searchInput"
+        placeholder="Search for a movie, tv show, person..."
       />
       <Button variant="light" className="border-0 p-0 position-absolute fs-4 top-0 start-0" type="submit">{'\u2315'}</Button>
     </Form>
