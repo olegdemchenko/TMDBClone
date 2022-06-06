6c76630fe7e8222ba0581ea114e108263e6f1c09
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-06-06 16:41:54 +0300

    Delete utility functions from Pagination comp file

--- src/features/search/Pagination.tsx
+++ src/features/search/Pagination.tsx
@@ -1,33 +1,7 @@
 import React from 'react';
 import Pagination from 'react-bootstrap/Pagination';
 import { useSearchParams } from 'react-router-dom';
-
-function isPageVisible(page: number, currentPage: number, totalPages: number) {
-  const visiblePagesFromStart = 7;
-  const visiblePageSiblings = 3;
-  const visibleBorderPages = 2;
-  if (currentPage <= visiblePagesFromStart - visiblePageSiblings && page <= visiblePagesFromStart) {
-    return true;
-  }
-  if (currentPage <= visiblePagesFromStart && page <= currentPage + visiblePageSiblings) {
-    return true;
-  }
-  if (currentPage > totalPages - (visiblePagesFromStart - visiblePageSiblings)
-    && page > totalPages - visiblePagesFromStart) {
-    return true;
-  }
-  if (currentPage > totalPages - visiblePagesFromStart
-    && page > totalPages - (visiblePagesFromStart - visiblePageSiblings)) {
-    return true;
-  }
-  return page <= visibleBorderPages || page > totalPages - visibleBorderPages
-    || (page >= currentPage - visiblePageSiblings && page <= currentPage + visiblePageSiblings);
-}
-
-export function findVisiblePages(currentPage: number, totalPages: number) {
-  return Array.from({ length: totalPages }, (_, i) => i + 1)
-    .filter((page) => isPageVisible(page, currentPage, totalPages));
-}
+import { findVisiblePages } from '../../common/utils';
 
 interface PaginationProps {
   currentPage: number;
