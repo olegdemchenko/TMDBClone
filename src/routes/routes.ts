ae3f64118db32a69ee8acd3191f5f7929888277e
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-06-06 16:20:38 +0300

    Add page param to getMultiSearch route getter

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,16 +2,17 @@ const origin = 'https://api.themoviedb.org';
 const version = '3';
 const APIKey = '93e4398b13ae3ceac59da26477413183';
 
-type RouteFunc = (...params: (string | null)[]) => URL;
+type RouteFunc = (...params: (string | number | null)[]) => URL;
 
 export const getUrl = (...path: string[]) => new URL([origin, version, ...path].join('/'));
 
 const routes = {
-  getMultiSearch: (query:string | null) => {
+  getMultiSearch: (query:string | null, page: number) => {
     const url = getUrl('search', 'multi');
     if (query) {
       url.searchParams.append('query', query);
     }
+    url.searchParams.append('page', String(page));
     return url;
   },
 };
