b4e3fb27f1067923aa8f1f40ef0559ff5b7e9b87
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-06-07 15:42:29 +0300

    Extract tests related to Search into separate file

--- __tests__/mainPage.test.tsx
+++ __tests__/mainPage.test.tsx
@@ -4,11 +4,8 @@ import {
   waitFor,
   fireEvent,
 } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import React from 'react';
 import App from '../src/app/App';
-import server from '../__mocks__/server';
-import SearchQueries from '../__mocks__/testQueries';
 import renderWithWrapper from './utils';
 
 test('check header behavior', async () => {
@@ -18,38 +15,6 @@ test('check header behavior', async () => {
   fireEvent.scroll(window, { target: { scrollY: 300 } });
   await waitFor(() => {
     expect(header).toHaveClass('hidden');
-  });
-});
-
-describe('check fetching data from API', () => {
-  beforeAll(() => server.listen());
-
-  afterEach(() => server.resetHandlers());
-
-  afterAll(() => server.close());
-
-  test('check successful search', async () => {
-    renderWithWrapper(<App />);
-    const searchInput = screen.getByPlaceholderText(/search/i);
-    const searchBtn = screen.getByRole('button', { name: /search/i });
-    await userEvent.type(searchInput, SearchQueries.MultiSearch);
-    await userEvent.click(searchBtn);
-    await waitFor(() => {
-      expect(window.location.pathname).toBe('/search');
-    });
-    await screen.findByRole('heading', { name: /search results/i });
-  });
-
-  test('check error handling after search request is sent', async () => {
-    renderWithWrapper(<App />);
-    const searchInput = screen.getByPlaceholderText(/search/i);
-    const searchBtn = screen.getByRole('button', { name: /search/i });
-    await userEvent.type(searchInput, SearchQueries.MultiSearchError);
-    await userEvent.click(searchBtn);
-    await waitFor(() => {
-      expect(window.location.pathname).toBe('/search');
-    });
-    await screen.findByRole('alert');
   });
 });
 
