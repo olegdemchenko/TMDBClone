7ab372081a91497d4957fdf04727ff01d60e8f1e
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-06-09 15:52:11 +0300

    Add specific data to mock fetching popular movies

--- __mocks__/server/handlers.ts
+++ __mocks__/server/handlers.ts
@@ -3,7 +3,8 @@ import {
   MultiSearchResults, 
   TVSearchInfo, 
   Error,
-  MediaTypes
+  MediaTypes,
+  MovieSearchInfo
 } from '../../src/app/APIInterfaces';
 import { getUrl } from '../../src/routes/routes';
 import SearchQueries from '../testQueries';
@@ -26,6 +27,26 @@ const movieSearchRes: TVSearchInfo = {
   name: 'Bradley',
   original_name: 'Bradley',
 };
+
+export const movieListResult: MovieSearchInfo = {
+  poster_path: "/lIv1QinFqz4dlp5U4lQ6HaiskOZ.jpg",
+  adult: false,
+  overview: "Under the direction of a ruthless instructor, a talented young drummer begins to pursue perfection at any cost, even his humanity.",
+  release_date: "2014-10-10",
+  genre_ids: [
+    18,
+    10402
+  ],
+  id: 244786,
+  media_type: MediaTypes.movie,
+  original_language: "en",
+  title: "Whiplash",
+  backdrop_path: "/6bbZ6XyvgfjhQwbplnUh1LSj1ky.jpg",
+  popularity: 10.776056,
+  vote_count: 2059,
+  video: false,
+  vote_average: 8.29
+}
 
 const multiSearchRes: MultiSearchResults = {
   page: 1,
@@ -56,7 +77,7 @@ const handlers = [
   rest.get(getUrl('movie', 'popular').toString(), (req, res, ctx) => {
     const resp: MultiSearchResults = {
       page: 1,
-      results: Array(20).fill(movieSearchRes).map((movie) => ({ ...movie, id: Math.random() })),
+      results: Array(20).fill(movieListResult).map((movie) => ({ ...movie, id: Math.random() })),
       total_pages: 1,
       total_results: 100
     }
