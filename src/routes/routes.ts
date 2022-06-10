419b53aab9a51aa242c3337449a9f879f58e68e0
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-06-10 15:21:27 +0300

    Add routes for api data of movies diff categories

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,21 +1,24 @@
 const origin = 'https://api.themoviedb.org/3';
 const APIKey = '93e4398b13ae3ceac59da26477413183';
 
-type RouteFunc = (...params: (string | number | null)[]) => URL;
-
-type Routes = {
-  [Key in keyof typeof paths as `get${Capitalize<Key>}`]: RouteFunc
-};
-
 export const paths = {
   multiSearch: `${origin}/search/multi`,
   popularMovies: `${origin}/movie/popular`,
+  nowPlayingMovies: `${origin}/movie/now_playing`,
+  topRatedMovies: `${origin}/movie/top_rated`,
+  upcomingMovies: `${origin}/movie/upcoming`,
 };
 
 export const imagePaths = {
   searchResIcon: 'https://image.tmdb.org/t/p/w92/',
   carouselSlidePoster: 'https://image.tmdb.org/t/p/w154/',
 };
+
+type Routes = {
+  [Key in keyof typeof paths as `get${Capitalize<Key>}`]: RouteFunc
+};
+
+type RouteFunc = (...params: (string | number | null)[]) => URL;
 
 function setSearchParam(url: URL, name: string, value: string) {
   url.searchParams.set(name, value);
@@ -33,6 +36,9 @@ const routes: Routes = {
     setSearchParam(setSearchParam(new URL(paths.multiSearch), 'query', query), 'page', String(page))
   ),
   getPopularMovies: (page: number) => setSearchParam(new URL(paths.popularMovies), 'page', String(page)),
+  getNowPlayingMovies: (page: number) => setSearchParam(new URL(paths.nowPlayingMovies), 'page', String(page)),
+  getTopRatedMovies: (page: number) => setSearchParam(new URL(paths.topRatedMovies), 'page', String(page)),
+  getUpcomingMovies: (page: number) => setSearchParam(new URL(paths.upcomingMovies), 'page', String(page)),
 };
 
 const ApiKeyRoutes = Object.fromEntries(
