702f0d4e8d17af3a5f631a5b49cc6ba5623f0017
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-06-14 18:42:49 +0300

    Set up i18n for app purposes

---
+++ src/i18n.ts
@@ -0,0 +1,14 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import locales from './locales';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    lng: 'en',
+    fallbackLng: 'en',
+    debug: true,
+    resources: locales,
+  });
+
+export default i18n;
