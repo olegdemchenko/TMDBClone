4ff25c1ae94e3243dc6e1dfe5b49cc036c8e4402
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-06-15 11:36:11 +0300

    Add translations to Main component

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import Search from '../search/Search';
 import Carousel from '../carousel/Carousel';
 import routes from '../../routes/routes';
 
 function MainPage() {
+  const { t } = useTranslation('main');
   return (
     <>
       <Search mode="main" />
       <Carousel
-        heading="What's popular"
+        heading={t('movies.popular')}
         slidesDataLink={routes.getPopularMovies(1)}
       />
       <Carousel
-        heading="Upcoming movies"
+        heading={t('movies.upcoming')}
         slidesDataLink={routes.getUpcomingMovies(1)}
       />
       <Carousel
-        heading="Top rated movies"
+        heading={t('movies.top')}
         slidesDataLink={routes.getTopRatedMovies(1)}
       />
       <Carousel
-        heading="Now playing movies"
+        heading={t('movies.playing')}
         slidesDataLink={routes.getNowPlayingMovies(1)}
       />
     </>
--- src/locales/en/index.ts
+++ src/locales/en/index.ts
@@ -1,5 +1,7 @@
 import header from './header.json';
+import main from './main.json';
 
 export default {
   header,
+  main,
 };
