d4164cb5cb101842e716db38ec438e9a198bd6bb
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-06-16 14:46:07 +0300

    Add emotion styles instead scss to Footer comp

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { css } from '@emotion/react';
+import { ThemeColors } from '../../common/styles';
 import logo from '../../assets/img/footerLogo.svg';
+
+const footerStyles = css({
+  backgroundColor: ThemeColors.darkBlue,
+  textAlign: 'center',
+  img: {
+    width: '50%',
+  },
+});
 
 function Footer() {
   return (
-    <footer className="p-4 footer">
+    <footer className="p-4" css={footerStyles}>
       <img src={logo} alt="footer logo" />
     </footer>
   );
