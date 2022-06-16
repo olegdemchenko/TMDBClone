6e033713591f4c4286b7a9004d96ce881b5c1904
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-06-16 16:34:25 +0300

    Use emotion styles instead scss in Search comp

--- src/components/search/MainPageSearch.tsx
+++ src/components/search/MainPageSearch.tsx
@@ -3,12 +3,43 @@ import { FormikProps } from 'formik';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { css } from '@emotion/react';
 import { useTranslation } from 'react-i18next';
+import { ThemeColors } from '../../common/styles';
 
 export interface SearchFormValues {
   searchQuery: string;
 }
 export type SearchUIProps = Pick<FormikProps<SearchFormValues>, 'handleSubmit' | 'handleChange' | 'errors'>;
+
+const searchStyles = css({
+  position: 'relative',
+  paddingTop: '5rem',
+  paddingBottom: '5rem',
+  backgroundColor: ThemeColors.darkBlue,
+  color: 'white',
+  '&:after': {
+    position: 'absolute',
+    top: -100,
+    left: 0,
+    content: '""',
+    width: '100%',
+    height: 100,
+    backgroundColor: ThemeColors.darkBlue,
+  },
+});
+
+const sloganStyles = css({
+  paddingBottom: '4rem',
+});
+
+const buttonStyles = css({
+  backgroundColor: ThemeColors.lightBlue,
+  '&:hover': {
+    backgroundColor: ThemeColors.lightBlue,
+    color: 'black',
+  },
+});
 
 function MainPageSearch({
   handleChange,
@@ -17,9 +48,13 @@ function MainPageSearch({
 }: SearchUIProps) {
   const { t } = useTranslation('search');
   return (
-    <Container fluid="lg" className="px-4 search-main">
+    <Container
+      fluid="lg"
+      className="px-4"
+      css={searchStyles}
+    >
       <h1 className="fw-bold mb-0">{t('main.greeting')}</h1>
-      <h2>{t('main.slogan')}</h2>
+      <h2 css={sloganStyles}>{t('main.slogan')}</h2>
       <Form onSubmit={handleSubmit}>
         <Form.Group className="position-relative">
           <Form.Control
@@ -32,7 +67,13 @@ function MainPageSearch({
           <Form.Control.Feedback type="invalid">
             {errors.searchQuery}
           </Form.Control.Feedback>
-          <Button type="submit" className="py-2 px-4 border-0 position-absolute top-0 end-0 rounded-pill fw-bold">{t('main.search')}</Button>
+          <Button
+            type="submit"
+            className="py-2 px-4 border-0 position-absolute top-0 end-0 rounded-pill fw-bold"
+            css={buttonStyles}
+          >
+            {t('main.search')}
+          </Button>
         </Form.Group>
       </Form>
     </Container>
