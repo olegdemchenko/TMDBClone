c52ccdedbc5126a6f5de40bb400ce184e5386544
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-06-20 11:12:19 +0300

    Connect Redux store to the app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,18 +3,22 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 import Root from '../components/layout/Layout';
 import Main from '../components/main/Main';
 import Results from '../components/results/Results';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Root />}>
-        <Route index element={<Main />} />
-        <Route path="search" element={<Results />} />
-      </Route>
-    </Routes>
+    <Provider store={store}>
+      <Routes>
+        <Route path="/" element={<Root />}>
+          <Route index element={<Main />} />
+          <Route path="search" element={<Results />} />
+        </Route>
+      </Routes>
+    </Provider>
   );
 }
 
