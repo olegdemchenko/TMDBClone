ed5ba5f786e04cd3be0bb247bf029745ec2612b7
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-06-21 14:25:24 +0300

    Extract header dropdowns text from i18n

--- src/locales/en/index.ts
+++ src/locales/en/index.ts
@@ -1,10 +1,8 @@
-import header from './header.json';
 import main from './main.json';
 import search from './search.json';
 import results from './results.json';
 
 export default {
-  header,
   main,
   search,
   results,
