ef96ae735ae5824fca09eab87156b3bc0450d745
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-06-21 15:38:28 +0300

    Navigate after header dropdown menu item clicked

--- src/components/header/DropdownMenu.tsx
+++ src/components/header/DropdownMenu.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Dropdown from 'react-bootstrap/Dropdown';
 import _ from 'lodash';
 import { capitalize } from '../../common/utils';
 
 interface DropdownProps {
+  path: string,
   name: string,
   list: string[],
 }
 
-function DropdownMenu({ name, list }: DropdownProps) {
+function getDropdownItemPath(path: string, dropdownItem: string) {
+  if (dropdownItem === 'popular') {
+    return `/${path}/`;
+  }
+  return `/${path}/${dropdownItem.replace(/ /g, '-')}`;
+}
+
+function DropdownMenu({
+  name,
+  list,
+  path,
+}: DropdownProps) {
+  const navigate = useNavigate();
   return (
     <Dropdown>
       <Dropdown.Toggle className="border-0 pb-0 shadow-none bg-transparent dropdown-btn">
@@ -16,7 +30,12 @@ function DropdownMenu({ name, list }: DropdownProps) {
       </Dropdown.Toggle>
       <Dropdown.Menu>
         {list.map((item) => (
-          <Dropdown.Item key={_.uniqueId()} href="#/action-1">{capitalize(item)}</Dropdown.Item>
+          <Dropdown.Item
+            key={_.uniqueId()}
+            onClick={() => navigate(getDropdownItemPath(path, item))}
+          >
+            {capitalize(item)}
+          </Dropdown.Item>
         ))}
       </Dropdown.Menu>
     </Dropdown>
--- src/components/header/Menu.tsx
+++ src/components/header/Menu.tsx
@@ -4,15 +4,28 @@ import DropdownMenu from './DropdownMenu';
 
 function Menu() {
   const menu = {
-    movies: ['popular', 'now playing', 'top rated', 'upcoming'],
-    'tv shows': ['popular', 'airing today', 'on tv', 'top rated'],
-    people: ['popular people'],
+    movie: ['popular', 'now playing', 'top rated', 'upcoming'],
+    tv: ['popular', 'airing today', 'on tv', 'top rated'],
+    person: ['popular people'],
     more: ['discussions', 'leaderboard', 'support', 'api'],
   };
+
+  const dropdownNames = {
+    movie: 'movies',
+    tv: 'tv shows',
+    person: 'people',
+    more: 'more',
+  };
+
   return (
     <div className="d-flex">
-      {Object.entries(menu).map(([name, list]) => (
-        <DropdownMenu key={_.uniqueId()} name={name} list={list} />
+      {Object.entries(menu).map(([path, list]) => (
+        <DropdownMenu
+          key={_.uniqueId()}
+          path={path}
+          name={dropdownNames[path as keyof typeof dropdownNames]}
+          list={list}
+        />
       ))}
     </div>
   );
