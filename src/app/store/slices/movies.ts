4e104359a9784f3e3adf18d855a13399709d5f68
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-06-24 16:41:40 +0300

    Add store slice rel to movies data

---
+++ src/app/store/slices/movies.ts
@@ -0,0 +1,34 @@
+/* eslint no-param-reassign: "off" */
+import { createSlice, createEntityAdapter, createSelector } from '@reduxjs/toolkit';
+import { RootState } from '../store';
+import { MovieListItem } from '../../APIInterfaces';
+import { tmdbApi } from '../api';
+
+interface MoviesByCategories {
+  popularIds: number[]
+}
+
+const movieAdapter = createEntityAdapter<MovieListItem>();
+const moviesSlice = createSlice({
+  name: 'movies',
+  initialState: movieAdapter.getInitialState<MoviesByCategories>({
+    popularIds: [],
+  }),
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addMatcher(
+      tmdbApi.endpoints.getPopularMovies.matchFulfilled,
+      (state, { payload }) => {
+        state.popularIds = [...state.popularIds, ...payload.results.map(({ id }) => id)];
+        movieAdapter.setMany(state, payload.results);
+      },
+    );
+  },
+});
+
+export const selectPopularMovies = createSelector(
+  (state: RootState) => state.movies,
+  (movies) => movies.popularIds.reduce((acc, id) => [...acc, movies.entities[id]], []),
+);
+
+export default moviesSlice;
--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit/';
 import { tmdbApi } from './api';
+import moviesSlice from './slices/movies';
 
 export const store = configureStore({
   reducer: {
     [tmdbApi.reducerPath]: tmdbApi.reducer,
+    movies: moviesSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => (
     getDefaultMiddleware().concat(tmdbApi.middleware)
