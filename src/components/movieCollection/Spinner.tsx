479b04d33b9b45f2facc10c00f939ec1a486013e
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-06-27 17:21:33 +0300

    Add custom spinner for MovieCollection comp

---
+++ src/components/movieCollection/Spinner.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { css } from '@emotion/react';
+import Spinner from 'react-bootstrap/Spinner';
+
+const collectionSpinnerStyles = css({
+  position: 'fixed',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: 'rgba(3, 37, 65, 0.5)',
+  zIndex: 10000,
+});
+
+const spinnerSizeStyles = css({
+  width: '5rem',
+  height: '5rem',
+});
+
+function CollectionSpinner() {
+  return <div css={collectionSpinnerStyles}><Spinner css={spinnerSizeStyles} animation="border" variant="primary" /></div>;
+}
+
+export default CollectionSpinner;
