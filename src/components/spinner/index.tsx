5931578dae2b7565786e0f6c3ba48c6c1fe12fa8
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-06-27 10:09:02 +0300

    Add align-items center to Spinner

--- src/components/spinner/index.tsx
+++ src/components/spinner/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Spinner from 'react-bootstrap/Spinner';
 
 function GallerySpinner() {
-  return <div className="d-flex justify-content-center py-3"><Spinner animation="grow" /></div>;
+  return <div className="d-flex justify-content-center align-items-center py-3"><Spinner animation="grow" /></div>;
 }
 
 export default GallerySpinner;
