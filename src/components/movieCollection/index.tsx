d2a648059d4b0e8af59d8ee7166734d88ddadf5b
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-06-29 12:35:25 +0300

    Skip changing content that is rendered MovieColl

--- src/components/movieCollection/index.tsx
+++ src/components/movieCollection/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Alert from 'react-bootstrap/Alert';
 import useUpdatePageAfterScroll from '../../common/hooks/useUpdatePageAfterScroll';
 import { SendQuery } from '../../common/hooks/useStoredUseQuery';
-import { isDataDefined } from '../../common/utils';
-import { MovieListItem } from '../../app/APIInterfaces';
 import Wrapper from '../gallery/GalleryWrapper';
 import Spinner from './spinner';
+import ErrorMessage from './ErrorMessage';
 import GalleryItemsList from '../gallery/GalleryItemsList';
 
 interface MovieCollectionProps {
@@ -21,32 +19,19 @@ function MovieCollection({
   const {
     isError,
     isFetching,
-    isSuccess,
     error,
     data,
   } = sendQuery(page);
-  let content: React.ReactNode;
-  if (isFetching) {
-    content = <Spinner />;
-  }
-  if (isError) {
-    content = <Alert variant="danger">{ error?.message ?? 'Unknown error has happened.' }</Alert>;
-  }
-  if (isSuccess) {
-    content = (
-      <>
-        <h3 className="m-0 pb-4">{heading}</h3>
-        <GalleryItemsList
-          mode="multiline"
-          heading={heading}
-          list={data as (MovieListItem)[]}
-        />
-      </>
-    );
-  }
   return (
     <Wrapper mode="screen">
-      { content }
+      { isFetching ? <Spinner /> : null }
+      { isError ? <ErrorMessage message={error?.message ?? 'Unknown error has happened.'} /> : null }
+      <h3 className="m-0 pb-4">{heading}</h3>
+      <GalleryItemsList
+        mode="multiline"
+        heading={heading}
+        list={data}
+      />
     </Wrapper>
   );
 }
