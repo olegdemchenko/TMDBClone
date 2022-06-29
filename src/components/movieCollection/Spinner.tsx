077315198ad1f482998d5212edf9db493cce5530
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-06-29 12:04:47 +0300

    Update MovieColl Spinner to behave like a modal

--- src/components/movieCollection/Spinner.tsx
+++ src/components/movieCollection/Spinner.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
+import Modal from 'react-bootstrap/Modal';
 import { css } from '@emotion/react';
 import Spinner from 'react-bootstrap/Spinner';
 
-const collectionSpinnerStyles = css({
-  position: 'fixed',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-  backgroundColor: 'rgba(3, 37, 65, 0.5)',
-  zIndex: 10000,
+const modalStyles = css({
+  '& .modal-content': {
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    border: 'none',
+  },
 });
 
 const spinnerSizeStyles = css({
@@ -21,7 +17,11 @@ const spinnerSizeStyles = css({
 });
 
 function CollectionSpinner() {
-  return <div css={collectionSpinnerStyles} data-testid="collectionSpinner"><Spinner css={spinnerSizeStyles} animation="border" variant="primary" /></div>;
+  return (
+    <Modal css={modalStyles} show centered>
+      <Spinner css={spinnerSizeStyles} animation="border" variant="primary" />
+    </Modal>
+  );
 }
 
 export default CollectionSpinner;
