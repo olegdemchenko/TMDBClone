6aef07044aef0bb058052b6b47df4b6b9b64e9c5
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-06-30 15:10:14 +0300

    Add hook which uses RTKQ cached data

---
+++ src/common/hooks/useCachedQueryData.ts
@@ -0,0 +1,46 @@
+import { QueryHooks } from '@reduxjs/toolkit/dist/query/react/buildHooks';
+import { createSelector } from '@reduxjs/toolkit';
+import { QueryDefinition } from '@reduxjs/toolkit/dist/query';
+import { ApiEndpointQuery } from '@reduxjs/toolkit/dist/query/core/module';
+import { useSelector } from 'react-redux';
+import { MovieListResult } from '../../app/APIInterfaces';
+import { tmdbApi } from '../../app/store/api';
+import axiosBaseQuery from '../../app/store/api/axiosBaseQuery';
+
+type QueryHookParams = number;
+
+type MovieEndpointQuery = QueryDefinition<
+QueryHookParams,
+typeof axiosBaseQuery,
+any,
+MovieListResult>;
+
+export type SendQuery = ReturnType<typeof useCachedQueryData>;
+
+type Endpoint = ApiEndpointQuery<
+MovieEndpointQuery,
+{ [K in Exclude<keyof typeof tmdbApi.endpoints, 'getMultiSearch'>]: MovieEndpointQuery }
+> & QueryHooks<MovieEndpointQuery>;
+
+const selectCachedData = (endpoint: Endpoint, page: number) => {
+  const selectors = Array.from(
+    { length: page },
+    (v, index) => endpoint.select(index + 1),
+  );
+  return createSelector(
+    selectors,
+    (...cachedResults) => (
+      cachedResults.reduce((acc, { data }) => [...acc, ...(data?.results ?? [])], [])
+    ),
+  );
+};
+
+export default function useCachedQueryData(
+  endpoint: Endpoint,
+) {
+  return (param: QueryHookParams) => {
+    const queryRes = endpoint.useQuery(param);
+    const storedData = useSelector(selectCachedData(endpoint, param));
+    return { ...queryRes, data: storedData };
+  };
+}
