2104ec9ae5f2fd9b197df5aa5031d79ef0f7b132
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-07-05 15:41:55 +0300

    Add Filter comp common styles

---
+++ src/components/collection/filter/styles.ts
@@ -0,0 +1,43 @@
+import { css } from '@emotion/react';
+import { ThemeColors } from '../../../common/styles';
+
+export const headingStyles = css({
+  display: 'block',
+  margin: 0,
+  padding: '0 0 10px 0',
+});
+
+export const selectStyles = css({
+  display: 'block',
+  width: '100%',
+  position: 'relative',
+  padding: 5,
+  border: 'none',
+  borderRadius: 5,
+  outline: 0,
+  backgroundColor: ThemeColors.lightGray,
+});
+
+export const containerStyles = css({
+  borderBottom: `1px solid ${ThemeColors.lightGray}`,
+  padding: '1.25rem 1.25rem 1rem 1.25rem',
+});
+
+export const noBorderStyles = css({
+  border: 'none',
+});
+
+export const buttonSelectedStyles = css({
+  border: `1px solid ${ThemeColors.lightBlue}`,
+  backgroundColor: ThemeColors.lightBlue,
+  color: 'white',
+});
+
+export const buttonStyles = css({
+  height: 30,
+  verticalAlign: 'middle',
+  border: `1px solid ${ThemeColors.gray}`,
+  borderRadius: 15,
+  padding: '0 15px',
+  '&:hover': buttonSelectedStyles,
+});
