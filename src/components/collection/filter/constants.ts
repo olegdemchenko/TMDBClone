5346e58eb9d9e55a6693d0cb19821a79e0afeabf
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-07-06 12:44:37 +0300

    Add a file with Filter param constants

---
+++ src/components/collection/filter/constants.ts
@@ -0,0 +1,32 @@
+export enum SortAlg {
+  popularityDesc = 'popularityDesc',
+  popularityAsc = 'popularityAsc',
+  ratingDesc = 'ratingDesc',
+  ratingAsc = 'ratingAsc',
+  releaseDateDesc = 'releaseDateDesc',
+  releaseDateAsc = 'releaseDateAsc',
+  titleAZ = 'titleAZ',
+  titleZA = 'titleAZ',
+}
+
+export enum Languages {
+  en = 'english',
+  zh = 'chinese',
+  hi = 'hindi',
+  es = 'spanish',
+  fr = 'French',
+  ar = 'arabic',
+  ru = 'russian',
+  pt = 'portuguese',
+  in = 'indonesian',
+}
+
+export enum UserVotes {
+  min = 0,
+  max = 500,
+}
+
+export enum UserScore {
+  min = 0,
+  max = 10,
+}
