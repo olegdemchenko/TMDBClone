6753a4a56c66bcc3141544c38767a1a2a6031c39
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-07-13 18:24:54 +0300

    Fix implementation bugs of Filter sort test

--- src/components/collection/filter.test.ts
+++ src/components/collection/filter.test.ts
@@ -16,13 +16,7 @@ function normalizeMonth(month: number) {
 function getRandomReleaseDate() {
   const timestamp = Number(Math.random().toFixed(13)) * 10000000000000;
   const date = new Date(timestamp);
-  return `
-    ${date.getFullYear}
-    -
-    ${addZeroToDate(normalizeMonth(date.getMonth()))}
-    -
-    ${addZeroToDate(date.getDate())}
-  `;
+  return `${date.getFullYear()}-${addZeroToDate(normalizeMonth(date.getMonth()))}-${addZeroToDate(date.getDate())}`;
 }
 
 type TestMovie = {
@@ -48,42 +42,6 @@ const movieList:TestMovie[] = Array(20).fill({}).map(() => ({
   title: _.uniqueId(),
   release_date: getRandomReleaseDate(),
 }));
-/*
-function sortMockedData(alg:SortAlg, movies:MovieListItem[]) {
-  type AlgorithmsMap = {
-    [key in SortAlg]: (a: MovieListItem, b: MovieListItem) => number;
-  };
-
-  type MovieDataValues = string | number | Date;
-  const getCompareableValue = (prop?: MovieDataValues) => prop ?? 1;
-
-  function compareValuesByAsc(value1?: MovieDataValues, value2?: MovieDataValues) {
-    if (getCompareableValue(value1) > getCompareableValue(value2)) {
-      return 1;
-    }
-    if (getCompareableValue(value1) < getCompareableValue(value2)) {
-      return -1;
-    }
-    return 0;
-  }
-
-  const Algs: AlgorithmsMap = {
-    [SortAlg.popularityAsc]: (a, b) => compareValuesByAsc(a?.popularity, b?.popularity),
-    [SortAlg.popularityDesc]: (a, b) => compareValuesByAsc(b?.popularity, a?.popularity),
-    [SortAlg.ratingAsc]: (a, b) => compareValuesByAsc(a?.vote_average, b?.vote_average),
-    [SortAlg.ratingDesc]: (a, b) => compareValuesByAsc(b?.vote_average, a?.vote_average),
-    [SortAlg.titleAZ]: (a, b) => compareValuesByAsc(a.title, b.title),
-    [SortAlg.titleZA]: (a, b) => compareValuesByAsc(b.title, a.title),
-    [SortAlg.releaseDateAsc]: (a, b) => (
-      compareValuesByAsc(new Date(a.release_date ?? 0), new Date(b.release_date ?? 0))
-    ),
-    [SortAlg.releaseDateDesc]: (a, b) => (
-      compareValuesByAsc(new Date(b.release_date ?? 0), new Date(a.release_date ?? 0))
-    ),
-  };
-
-  return movies.sort(Algs[alg]);
-}*/
 
 type PropertiesKeys = keyof MovieListItem;
 
@@ -104,23 +62,21 @@ function getPrimitiveValue(key: PropertiesKeys, container: TestMovie) {
   return key === 'release_date' ? new Date(container[key] as string).valueOf() : container[key];
 }
 
-const sortItems = (key: PropertiesKeys, direction: 'asc' | 'desc') => (
-  (a: TestMovie, b: TestMovie) => {
-    const valueA = getPrimitiveValue(key, a);
-    const valueB = getPrimitiveValue(key, b);
-    return direction === 'asc' ? compare(valueA, valueB) : compare(valueB, valueA);
-  }
-);
+const sortItems = (key: PropertiesKeys, direction: 'asc' | 'desc') => (a: TestMovie, b: TestMovie) => {
+  const valueA = getPrimitiveValue(key, a);
+  const valueB = getPrimitiveValue(key, b);
+  return direction === 'asc' ? compare(valueA, valueB) : compare(valueB, valueA);
+};
 
 test.each([
-  SortAlg.popularityAsc, [...movieList].sort(sortItems('popularity', 'asc')),
-  SortAlg.popularityDesc, [...movieList].sort(sortItems('popularity', 'desc')),
-  SortAlg.ratingAsc, [...movieList].sort(sortItems('vote_average', 'asc')),
-  SortAlg.ratingDesc, [...movieList].sort(sortItems('vote_average', 'asc')),
-  SortAlg.releaseDateAsc, [...movieList].sort(sortItems('release_date', 'asc')),
-  SortAlg.releaseDateDesc, [...movieList].sort(sortItems('release_date', 'desc')),
-  SortAlg.titleAZ, [...movieList].sort(sortItems('title', 'asc')),
-  SortAlg.titleZA, [...movieList].sort(sortItems('title', 'desc')),
+  [SortAlg.popularityAsc, [...movieList].sort(sortItems('popularity', 'asc'))],
+  [SortAlg.popularityDesc, [...movieList].sort(sortItems('popularity', 'desc'))],
+  [SortAlg.ratingAsc, [...movieList].sort(sortItems('vote_average', 'asc'))],
+  [SortAlg.ratingDesc, [...movieList].sort(sortItems('vote_average', 'desc'))],
+  [SortAlg.releaseDateAsc, [...movieList].sort(sortItems('release_date', 'asc'))],
+  [SortAlg.releaseDateDesc, [...movieList].sort(sortItems('release_date', 'desc'))],
+  [SortAlg.titleAZ, [...movieList].sort(sortItems('title', 'asc'))],
+  [SortAlg.titleZA, [...movieList].sort(sortItems('title', 'desc'))],
 ])('test sorting alg:%s', (sortAlg: SortAlg, presortedMovies: TestMovie[]) => {
   expect(filter({ sortAlg }, (movieList as unknown) as MovieListItem[])).toEqual(presortedMovies);
 });
