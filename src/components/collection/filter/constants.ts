e8a9c99f1d7caabaf3a5536c701d0099fb357280
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-07-19 16:56:11 +0300

    Add genres map to Filter/constants

--- src/components/collection/filter/constants.ts
+++ src/components/collection/filter/constants.ts
@@ -30,3 +30,24 @@ export enum UserScore {
   min = 0,
   max = 10,
 }
+
+export const genres = {
+  action: 28,
+  adventure: 12,
+  animation: 16,
+  comedy: 35,
+  documentary: 99,
+  drama: 18,
+  family: 10751,
+  fantasy: 14,
+  history: 36,
+  horror: 27,
+  music: 10402,
+  mystery: 9648,
+  romance: 10749,
+  scienceFiction: 878,
+  tvMovie: 10770,
+  thriller: 53,
+  war: 10752,
+  western: 37,
+};
