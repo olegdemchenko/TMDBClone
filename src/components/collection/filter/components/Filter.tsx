637edd1a3b85d6379cef798edabddf5139e45fbe
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-07-19 12:50:29 +0300

    Extract Filter menu into a separate component

--- src/components/collection/filter/components/Filter.tsx
+++ src/components/collection/filter/components/Filter.tsx
@@ -1,71 +1,15 @@
 import React, { useReducer, Reducer } from 'react';
-import Accordion from 'react-bootstrap/Accordion';
-import { useTranslation } from 'react-i18next';
-import { css } from '@emotion/react';
 import {
   FilterState,
   initialState,
   reducer,
   ReducerAction,
 } from '../state';
-import {
-  ThemeColors,
-  BorderRadiuses,
-} from '../../../../common/styles';
-import arrowRight from '../../../../assets/img/arrowRight.png';
-import arrowDown from '../../../../assets/img/arrowDown.png';
-import SortTypes from './SortTypes';
-import Dates from './Dates';
-import Genres from './Genres';
-import Languages from './Languages';
-import AgeLimitations from './AgeLimitations';
-import UserRatings from './UserRatings';
 import { MovieListItem } from '../../../../app/APIInfo';
 import GalleryItemsList from '../../../gallery/GalleryItemsList';
 import Container from './Container';
 import filter from '../filter';
-
-const filterWrapperStyles = css({
-  width: 300,
-  paddingRight: 25,
-  flexShrink: 0,
-});
-
-const accordionCustomStyles = css({
-  '& .according-header': {
-    fontWeight: 'bold',
-  },
-  '& .accordion-body': {
-    padding: 0,
-  },
-  '& .accordion-item': {
-    marginBottom: 10,
-    border: `2px solid ${ThemeColors.lightGray}`,
-    borderRadius: BorderRadiuses.medium,
-  },
-  '& .accordion-button': {
-    fontWeight: 'bold',
-    borderBottom: `1px solid ${ThemeColors.lightGray}`,
-    '&:focus': {
-      borderColor: ThemeColors.lightGray,
-      boxShadow: 'none',
-    },
-    '&:after': {
-      width: '1rem',
-      height: '1rem',
-      backgroundSize: '1rem',
-      backgroundImage: `url(${arrowRight})`,
-    },
-    '&:not(.collapsed)': {
-      color: 'black',
-      backgroundColor: '#fff',
-      boxShadow: 'none',
-      '&:after': {
-        backgroundImage: `url(${arrowDown})`,
-      },
-    },
-  },
-});
+import Menu from './Menu';
 
 interface FilterProps {
   heading: string;
@@ -76,7 +20,6 @@ function Filter({
   heading,
   list,
 }: FilterProps) {
-  const { t } = useTranslation('collection');
   const [
     state,
     dispatch,
@@ -84,36 +27,15 @@ function Filter({
 
   return (
     <Container heading={heading}>
-      <>
-        <div css={filterWrapperStyles}>
-          <Accordion defaultActiveKey={['0']} alwaysOpen css={accordionCustomStyles}>
-            <Accordion.Item eventKey="0">
-              <Accordion.Header>{t('filter.sort.name')}</Accordion.Header>
-              <Accordion.Body>
-                <SortTypes dispatch={dispatch} />
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="1">
-              <Accordion.Header>{t('filter.filters.name')}</Accordion.Header>
-              <Accordion.Body>
-                <Dates
-                  dispatch={dispatch}
-                  dates={state.dates}
-                />
-                <Genres />
-                <Languages />
-                <AgeLimitations />
-                <UserRatings />
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-        </div>
-        <GalleryItemsList
-          mode="multiline"
-          heading={heading}
-          list={filter(state, list)}
-        />
-      </>
+      <Menu
+        state={state}
+        dispatch={dispatch}
+      />
+      <GalleryItemsList
+        mode="multiline"
+        heading={heading}
+        list={filter(state, list)}
+      />
     </Container>
   );
 }
---
+++ src/components/collection/filter/components/Menu.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import Accordion from 'react-bootstrap/Accordion';
+import { css } from '@emotion/react';
+import {
+  FilterState,
+  ReducerAction,
+} from '../state';
+import SortTypes from './SortTypes';
+import Dates from './Dates';
+import Genres from './Genres';
+import Languages from './Languages';
+import AgeLimitations from './AgeLimitations';
+import UserRatings from './UserRatings';
+import {
+  ThemeColors,
+  BorderRadiuses,
+} from '../../../../common/styles';
+import arrowRight from '../../../../assets/img/arrowRight.png';
+import arrowDown from '../../../../assets/img/arrowDown.png';
+
+interface MenuProps {
+  state: FilterState;
+  dispatch: React.Dispatch<ReducerAction>
+}
+
+const filterWrapperStyles = css({
+  width: 300,
+  paddingRight: 25,
+  flexShrink: 0,
+});
+
+const accordionCustomStyles = css({
+  '& .according-header': {
+    fontWeight: 'bold',
+  },
+  '& .accordion-body': {
+    padding: 0,
+  },
+  '& .accordion-item': {
+    marginBottom: 10,
+    border: `2px solid ${ThemeColors.lightGray}`,
+    borderRadius: BorderRadiuses.medium,
+  },
+  '& .accordion-button': {
+    fontWeight: 'bold',
+    borderBottom: `1px solid ${ThemeColors.lightGray}`,
+    '&:focus': {
+      borderColor: ThemeColors.lightGray,
+      boxShadow: 'none',
+    },
+    '&:after': {
+      width: '1rem',
+      height: '1rem',
+      backgroundSize: '1rem',
+      backgroundImage: `url(${arrowRight})`,
+    },
+    '&:not(.collapsed)': {
+      color: 'black',
+      backgroundColor: '#fff',
+      boxShadow: 'none',
+      '&:after': {
+        backgroundImage: `url(${arrowDown})`,
+      },
+    },
+  },
+});
+
+function Menu({
+  state,
+  dispatch,
+}: MenuProps) {
+  const { t } = useTranslation('collection');
+  return (
+    <div css={filterWrapperStyles}>
+      <Accordion defaultActiveKey={['0']} alwaysOpen css={accordionCustomStyles}>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>{t('filter.sort.name')}</Accordion.Header>
+          <Accordion.Body>
+            <SortTypes dispatch={dispatch} />
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>{t('filter.filters.name')}</Accordion.Header>
+          <Accordion.Body>
+            <Dates
+              dispatch={dispatch}
+              dates={state.dates}
+            />
+            <Genres />
+            <Languages />
+            <AgeLimitations />
+            <UserRatings />
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </div>
+  );
+}
+
+export default Menu;
