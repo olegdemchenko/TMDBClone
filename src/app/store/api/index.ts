907f354462a5d6f5983df4d5a64bcaa7fac43a50
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-07-20 15:37:20 +0300

    Add TV Shows api endpoints

--- src/app/store/api/index.ts
+++ src/app/store/api/index.ts
@@ -2,6 +2,7 @@ import { createApi } from '@reduxjs/toolkit/query/react';
 import {
   MultiSearchResults,
   MovieListResult,
+  TVListResult,
 } from '../../APIInfo';
 import axiosBaseQuery from './axiosBaseQuery';
 
@@ -31,6 +32,18 @@ export const tmdbApi = createApi({
     }),
     getUpcomingMovies: builder.query<MovieListResult, number>({
       query: (page) => ({ url: '/movie/upcoming', method: 'get', params: { page } }),
+    }),
+    getPopularTVShows: builder.query<TVListResult, number>({
+      query: (page) => ({ url: '/tv/popular', method: 'get', params: { page } }),
+    }),
+    getAiringTVShows: builder.query<TVListResult, number>({
+      query: (page) => ({ url: '/tv/airing_today', method: 'get', params: { page } }),
+    }),
+    getOnTVShows: builder.query<TVListResult, number>({
+      query: (page) => ({ url: '/tv/on_the_air', method: 'get', params: { page } }),
+    }),
+    getTopRatedTVShows: builder.query<TVListResult, number>({
+      query: (page) => ({ url: '/tv/top_rated', method: 'get', params: { page } }),
     }),
   }),
 });
