6f740e231497e302fb7c9d38aed30ecb9a6664e9
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-08-18 12:29:19 +0300

    Delete useless routes utilities

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -21,33 +21,3 @@ export const imagePaths = {
     large: 'https://image.tmdb.org/t/p/w300/',
   },
 };
-
-type Routes = {
-  [Key in keyof typeof paths as `get${Capitalize<Key>}`]: RouteFunc;
-};
-
-type RouteFunc = (...params: (string | number | null)[]) => URL;
-
-function setSearchParam(url: URL, name: string, value: string) {
-  url.searchParams.set(name, value);
-  return url;
-}
-
-const routes: Routes = {
-  getMultiSearch: (query: string, page: number) =>
-    setSearchParam(
-      setSearchParam(new URL(paths.multiSearch), 'query', query),
-      'page',
-      String(page)
-    ),
-  getPopularMovies: (page: number) =>
-    setSearchParam(new URL(paths.popularMovies), 'page', String(page)),
-  getNowPlayingMovies: (page: number) =>
-    setSearchParam(new URL(paths.nowPlayingMovies), 'page', String(page)),
-  getTopRatedMovies: (page: number) =>
-    setSearchParam(new URL(paths.topRatedMovies), 'page', String(page)),
-  getUpcomingMovies: (page: number) =>
-    setSearchParam(new URL(paths.upcomingMovies), 'page', String(page)),
-};
-
-export default routes;
