339ae585e1c61e93a69c4d1c86a0931e5d35290d
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-08-18 18:51:05 +0300

    Restructure ResultsPageSearch subcomp

--- src/components/search/ResultsPageSearch/index.tsx
+++ src/components/search/ResultsPageSearch/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { useTranslation } from 'react-i18next';
-import { SearchUIProps } from './MainPageSearch';
+import { SearchUIProps } from '../MainPageSearch';
 
 function ResultsPageSearch({
   handleChange,
