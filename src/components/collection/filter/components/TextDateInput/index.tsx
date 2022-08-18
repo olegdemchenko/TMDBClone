745194fe4522200799dcb48e10b770fae861775f
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-08-18 15:50:15 +0300

    Restructure TextDateInput subcomponent

--- src/components/collection/filter/components/TextDateInput/index.tsx
+++ src/components/collection/filter/components/TextDateInput/index.tsx
@@ -1,31 +1,11 @@
 import React, { useState, useRef } from 'react';
-import { css } from '@emotion/react';
-import { ThemeColors, BorderRadiuses } from '../../../../common/styles';
 import {
   dateToStringWithDot,
   validateDate,
   parseDate,
-} from '../../../../common/utils';
-import { DateInputProps } from './DateInput';
-
-const textInputStyles = css({
-  width: '100%',
-  position: 'relative',
-  border: `2px solid ${ThemeColors.lightGray}`,
-  borderRadius: BorderRadiuses.small,
-  outline: 'none',
-  backgroundColor: 'transparent',
-  '&:focus': {
-    border: `2px solid ${ThemeColors.lightBlue}`,
-  },
-  zIndex: 1,
-});
-
-const errorInputStyles = css({
-  '&:focus': {
-    borderColor: ThemeColors.red,
-  },
-});
+} from '../../../../../common/utils';
+import { DateInputProps } from '../DateInput';
+import { inputStyles, errorStyles } from './styles';
 
 function TextDateInput({ date, setDate }: DateInputProps) {
   const inputRef = useRef(null);
@@ -54,7 +34,7 @@ function TextDateInput({ date, setDate }: DateInputProps) {
   const currentValue = selectValue(date, enteredDate);
   return (
     <input
-      css={[textInputStyles, isEnteredDateValid ? {} : errorInputStyles]}
+      css={[inputStyles, isEnteredDateValid ? {} : errorStyles]}
       placeholder='DD.MM.YYYY'
       value={currentValue}
       onChange={handleTextChange}
---
+++ src/components/collection/filter/components/TextDateInput/styles.ts
@@ -0,0 +1,21 @@
+import { css } from '@emotion/react';
+import { ThemeColors, BorderRadiuses } from '../../../../../common/styles';
+
+export const inputStyles = css({
+  width: '100%',
+  position: 'relative',
+  border: `2px solid ${ThemeColors.lightGray}`,
+  borderRadius: BorderRadiuses.small,
+  outline: 'none',
+  backgroundColor: 'transparent',
+  '&:focus': {
+    border: `2px solid ${ThemeColors.lightBlue}`,
+  },
+  zIndex: 1,
+});
+
+export const errorStyles = css({
+  '&:focus': {
+    borderColor: ThemeColors.red,
+  },
+});
