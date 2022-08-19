5c0fdcef7c3dad1b644a428bfe207114b14257f8
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-08-19 16:30:54 +0300

    Add interface rel to Popular People API resp

--- src/app/TMDBAPIInterfaces/people.ts
+++ src/app/TMDBAPIInterfaces/people.ts
@@ -1,10 +1,17 @@
 import { MovieListItemMedia, TVListItemMedia } from './search';
 
-export interface People {
+export interface PopularPerson {
   profile_path: string;
   adult: boolean;
   id: number;
   known_for: (MovieListItemMedia | TVListItemMedia)[];
   name: string;
   popularity: number;
+}
+
+export interface PopularPeopleResult {
+  page: number;
+  total_pages: number;
+  total_results: number;
+  results: PopularPerson[];
 }
