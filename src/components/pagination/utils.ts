1c10339fbf13a078ed17d9a196d3e5fda562fc69
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-08-19 12:42:44 +0300

    Delete useless if cond from isPageVisible func

--- src/components/pagination/utils.ts
+++ src/components/pagination/utils.ts
@@ -1,42 +1,37 @@
-function isPageVisible(page: number, currentPage: number, totalPages: number) {
+function isPageVisible(page: number, selectedPage: number, totalPages: number) {
   const visiblePagesFromStart = 7;
   const visiblePageSiblings = 3;
   const visibleBorderPages = 2;
-  if (
-    currentPage <= visiblePagesFromStart - visiblePageSiblings &&
-    page <= visiblePagesFromStart
-  ) {
-    return true;
-  }
-  if (
-    currentPage <= visiblePagesFromStart &&
-    page <= currentPage + visiblePageSiblings
-  ) {
-    return true;
-  }
-  if (
-    currentPage > totalPages - (visiblePagesFromStart - visiblePageSiblings) &&
-    page > totalPages - visiblePagesFromStart
-  ) {
-    return true;
-  }
-  if (
-    currentPage > totalPages - visiblePagesFromStart &&
-    page > totalPages - (visiblePagesFromStart - visiblePageSiblings)
-  ) {
-    return true;
-  }
-  return (
+
+  const isPageInStartVisibleRange =
+    (selectedPage <= visiblePagesFromStart - visiblePageSiblings &&
+      page <= visiblePagesFromStart) ||
+    (selectedPage <= visiblePagesFromStart &&
+      page <= selectedPage + visiblePageSiblings);
+
+  const isPageInEndVisibleRange =
+    (selectedPage >
+      totalPages - (visiblePagesFromStart - visiblePageSiblings) &&
+      page > totalPages - visiblePagesFromStart) ||
+    (selectedPage > totalPages - visiblePagesFromStart &&
+      page > totalPages - (visiblePagesFromStart - visiblePageSiblings));
+
+  const isPageInMiddleVisibleRange =
     page <= visibleBorderPages ||
     page > totalPages - visibleBorderPages ||
-    (page >= currentPage - visiblePageSiblings &&
-      page <= currentPage + visiblePageSiblings)
+    (page >= selectedPage - visiblePageSiblings &&
+      page <= selectedPage + visiblePageSiblings);
+
+  return (
+    isPageInStartVisibleRange ||
+    isPageInEndVisibleRange ||
+    isPageInMiddleVisibleRange
   );
 }
 
-function findVisiblePages(currentPage: number, totalPages: number) {
+function findVisiblePages(selectedPage: number, totalPages: number) {
   return Array.from({ length: totalPages }, (_, i) => i + 1).filter((page) =>
-    isPageVisible(page, currentPage, totalPages)
+    isPageVisible(page, selectedPage, totalPages)
   );
 }
 
