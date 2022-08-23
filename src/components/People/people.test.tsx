19bf79904e95bf54c534ba812ffdf6519055be4a
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-08-23 17:26:42 +0300

    Delete code that test Pagination from People test

--- src/components/People/people.test.tsx
+++ src/components/People/people.test.tsx
@@ -1,14 +1,12 @@
 import '@testing-library/jest-dom';
 import React from 'react';
 import { screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import { store } from '../../app/store/store';
 import { renderWithWrapper } from '../../common/utils';
 import People from '.';
 import {
   popularPerson,
-  pagesCount,
   peoplePerPage,
 } from '../../../__mocks__/server/handlers/getPopularPeople';
 
@@ -23,14 +21,4 @@ test('check rendering list of popular people', async () => {
       peoplePerPage
     );
   });
-
-  const lastPage = screen.getByRole('button', { name: String(pagesCount) });
-  userEvent.click(lastPage);
-
-  await waitFor(() => {
-    expect(screen.getAllByText(popularPerson.name as string).length).toBe(
-      peoplePerPage
-    );
-  });
-  expect(window.location.search).toBe(`?page=${pagesCount}`);
 });
