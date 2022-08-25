93f10e8a0f1a84d1ac6078991222c095e447fa80
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-08-25 11:31:38 +0300

    Connect store to MovieDetails test

--- src/components/MovieDetails/movieDetails.test.tsx
+++ src/components/MovieDetails/movieDetails.test.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import { screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import { renderWithWrapper, getMovieDetailsPath } from '../../common/utils';
+import { store } from '../../app/store/store';
 import { movieDetails as mockedDetails } from '../../../__mocks__/server/handlers/getMovieDetails';
 
 import MovieDetails from '.';
 
 test('check MovieDetails base content presence', async () => {
-  renderWithWrapper(<MovieDetails />, {
-    route: `/${getMovieDetailsPath(
-      mockedDetails.id,
-      mockedDetails.title as string
-    )}`,
-  });
+  renderWithWrapper(
+    <Provider store={store}>
+      <MovieDetails />
+    </Provider>,
+    {
+      route: `/${getMovieDetailsPath(
+        mockedDetails.id,
+        mockedDetails.title as string
+      )}`,
+    }
+  );
   expect(
     await screen.findByRole('heading', { name: mockedDetails.title })
   ).toBeInTheDocument();
