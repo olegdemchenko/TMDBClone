4ef91e8ff2f71d09362e5a3afac09dab22a96632
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-08-26 16:32:07 +0300

    Normalize html link elem styles

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './translations';
 import App from './app/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './app/normalize.css';
 
 const root = createRoot(document.getElementById('root') as HTMLBodyElement);
 root.render(
