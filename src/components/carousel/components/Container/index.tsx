83dedaaedbf8242001a06f51e7ef4b32288489bf
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-08-26 16:30:17 +0300

    Delete inner shadow styles from Carousel

--- src/components/carousel/components/Container/index.tsx
+++ src/components/carousel/components/Container/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import { innerShadowStyles, hiddenVerticalScrollbarStyles } from './styles';
+import { hiddenVerticalScrollbarStyles } from './styles';
 
 interface CarouselContainerProps {
   children: React.ReactNode;
@@ -8,7 +8,7 @@ interface CarouselContainerProps {
 
 function CarouselContainer({ children }: CarouselContainerProps) {
   return (
-    <Container fluid='lg' className='p-4 pe-0' css={innerShadowStyles}>
+    <Container fluid='lg' className='p-4 pe-0'>
       <div className='pb-4' css={hiddenVerticalScrollbarStyles}>
         {children}
       </div>
--- src/components/carousel/components/Container/styles.ts
+++ src/components/carousel/components/Container/styles.ts
@@ -1,18 +1,5 @@
 import { css } from '@emotion/react';
 
-export const innerShadowStyles = css({
-  position: 'relative',
-  '&::after': {
-    position: 'absolute',
-    content: '""',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: '4rem',
-    backgroundImage: 'linear-gradient(to right, transparent 93%, white)',
-  },
-});
-
 export const hiddenVerticalScrollbarStyles = css({
   overflowY: 'hidden',
 });
