5ed3ea5e965ced86edda0f3eac53a71d703512f4
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-09-03 13:02:00 +0300

    Apply Logo comp for Results comp subcomponents

--- src/components/results/components/MovieInfo/index.tsx
+++ src/components/results/components/MovieInfo/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import LinesEllipsis from 'react-lines-ellipsis';
-import notFound from './assets/img/notFound.png';
 import { imagePaths } from '../../../../routes';
 import { movieInfoStyle, logoWrapperStyle, infoWrapperStyle } from './styles';
+import Logo from '../../../Logo';
 
 interface ResultElemProps {
   title?: string;
@@ -12,11 +12,15 @@ interface ResultElemProps {
 }
 
 function MovieInfo({ title, logo, date, description }: ResultElemProps) {
-  const icon = logo ? `${imagePaths.searchResIcon}${logo}` : notFound;
   return (
     <div className='mb-3 d-flex overflow-hidden' css={movieInfoStyle}>
       <div className='flex-shrink-0' css={logoWrapperStyle}>
-        <img src={icon} alt='logo' />
+        <Logo
+          type='media'
+          borderRadius='left'
+          path={imagePaths.searchResIcon}
+          imgName={logo}
+        />
       </div>
       <div className='flex-grow-1' css={infoWrapperStyle}>
         <h6 className='m-0 fs-5 fw-bold'>{title}</h6>
--- src/components/results/components/PersonInfo/index.tsx
+++ src/components/results/components/PersonInfo/index.tsx
@@ -6,9 +6,8 @@ import {
   TVListItemMedia,
   MediaTypes,
 } from '../../../../app/TMDBAPIInterfaces';
-import { BorderRadiuses } from '../../../../common/styles';
-import noAvatar from './assets/img/noAvatar.jpg';
 import { imagePaths } from '../../../../routes';
+import Logo from '../../../Logo';
 
 interface PersonInfoProps {
   name: string;
@@ -19,19 +18,19 @@ interface PersonInfoProps {
 const photoStyles = css({
   width: 92,
   height: 92,
-  borderRadius: BorderRadiuses.medium,
-  img: {
-    marginTop: -15,
-  },
 });
 
 function PersonInfo({ name, avatar, works }: PersonInfoProps) {
   const { t } = useTranslation('results');
-  const photo = avatar ? `${imagePaths.searchResIcon}${avatar}` : noAvatar;
   return (
     <div className='d-flex mb-3'>
       <div className='flex-shrink-0 overflow-hidden' css={photoStyles}>
-        <img src={photo} alt='icon' />
+        <Logo
+          type='person'
+          borderRadius='all'
+          path={imagePaths.searchResIcon}
+          imgName={avatar}
+        />
       </div>
       <div className='d-flex flex-column info flex-grow-1 ps-3 justify-content-center'>
         <h6 className='fw-bold m-0 name'>{name}</h6>
