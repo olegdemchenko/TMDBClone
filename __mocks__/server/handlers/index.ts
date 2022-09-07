32a0de4036181e817f629774737953ad001ba277
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-09-07 10:53:29 +0300

    Delete useless MovieDetails test

--- __mocks__/server/handlers/index.ts
+++ __mocks__/server/handlers/index.ts
@@ -2,12 +2,10 @@ import getMultiSearch from './getMultiSearch';
 import getPopularMovies from './getPopularMovies';
 import getUpcomingMovies from './getUpcomingMovies';
 import getPopularPeople from './getPopularPeople';
-import getMovieDetails from './getMovieDetails';
 
 export default [
   getMultiSearch,
   getPopularMovies,
   getUpcomingMovies,
   getPopularPeople,
-  getMovieDetails,
 ];
