c9698e21c116a6c8df8206a17b7f45fd66e950da
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-09-20 11:01:07 +0300

    Add store to Media comp tests

--- src/components/Details/components/Media/media.test.tsx
+++ src/components/Details/components/Media/media.test.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { screen, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { Provider } from 'react-redux';
+import { store } from '../../../../app/store/store';
 import i18n from '../../../../translations';
 import Media from '.';
 
 test('check changing current content type', async () => {
   const currentTabAttr = 'data-selected';
-  render(<Media id={666} />);
+  render(
+    <Provider store={store}>
+      <Media id={666} />
+    </Provider>
+  );
   const videoTab = screen.getByRole('button', {
     name: i18n.t('details:videos'),
   });
