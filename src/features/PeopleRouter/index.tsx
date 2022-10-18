03d396757a6256f877ee9cb6becbb76ba7b98cc5
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-10-18 16:02:11 +0300

    Fix path to People list in People router

--- src/features/PeopleRouter/index.tsx
+++ src/features/PeopleRouter/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { peoplePathNames } from '../../routes';
+import { pathsSegments } from '../../routes';
 import People from '../People';
 import PersonDetails from '../PersonDetails';
 
 function PeopleRouter() {
   return (
     <Routes>
-      <Route path={peoplePathNames.popularPeople} element={<People />} />
+      <Route path={pathsSegments.popularPeople} element={<People />} />
       <Route path=':credentials' element={<PersonDetails />} />
     </Routes>
   );
