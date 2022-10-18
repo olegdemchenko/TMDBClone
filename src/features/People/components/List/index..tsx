7412d65a4e19b20d84b274f1e58e3b5bb3320801
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-10-18 16:05:38 +0300

    Add link to person details from People list

--- src/features/People/components/List/index..tsx
+++ src/features/People/components/List/index..tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import _ from 'lodash';
+import { Link } from 'react-router-dom';
 import { PopularPerson } from '../../../../app/TMDBAPIInterfaces';
 import ListItem from '../ListItem';
 import { listStyles } from './styles';
+import { detailsPaths } from '../../../../routes';
 
 interface ListProps {
   people: PopularPerson[];
@@ -11,13 +13,15 @@ interface ListProps {
 function List({ people }: ListProps) {
   return (
     <div css={listStyles}>
-      {people.map(({ name, known_for, profile_path }) => (
-        <ListItem
-          key={_.uniqueId()}
-          name={name}
-          knownFor={known_for}
-          photo={profile_path}
-        />
+      {people.map(({ id, name, known_for, profile_path }) => (
+        <Link to={detailsPaths.people(id, name ?? '')}>
+          <ListItem
+            key={_.uniqueId()}
+            name={name}
+            knownFor={known_for}
+            photo={profile_path}
+          />
+        </Link>
       ))}
     </div>
   );
